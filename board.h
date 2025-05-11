343bc7a3b6376b7a813af640d0f8278a972fd2e4
Arze Lu <lu_arze@outlook.com>
2025-05-11 10:59:59 -0700

    commut

--- board.h
+++ board.h
@@ -4,7 +4,7 @@
 
 typedef struct{
     uint8_t value;       // 0-9
-    uint8_t remaining;   // 0-9
+    uint8_t remainder;   // 0-9
     uint16_t candidates; // bitmask for valid candidates. 1 for valid
 } Cell;
 
--- sudoku.c
+++ sudoku.c
@@ -1,68 +1,73 @@
-// Patent number: 69420
-// blazinweed.org, all rights reserved
-// sponsored by Vaperjit
-// not allowed to distribute or monetize this code
-// try our flagship cannabis sample today (value at $60)!
-// interested? visit blazinweed.org/canny/item/highflyer/free-trial
+/// Patent number: 69420
+/// blazinweed.org, all rights reserved
+/// sponsored by Vaperjit
+/// not allowed to distribute or monetize this code
+/// try our flagship cannabis sample today (value at $60)!
+/// interested? visit blazinweed.org/canny/item/highflyer/free-trial
 
 //TODO: change the board system into a 1D array of 81 cells. cache locality
 #include <libs.h>
 #include <board.h>
-#include <helper.h>
+#include <helper.c>
 
-// Readjust the number of remaining possible candidates for all cells.
-// Assign the cell with a value if there's only one remaining.
-// The process is split into three for-loops internally to avoid race condition
-// DONE
-void constraint_propagation(Board **board){
+/// Readjust the number of remaining possible candidates for all cells.
+/// Assign the cell with a value if there's only one remaining.
+/// The process is split into three for-loops internally to avoid race condition
+/// Returns if there is a cell with just a single candidate remaining.
+/// DONE
+bool constraint_propagation(Board **board){
+    bool singles = false;
+
     #pragma omp parallel for
     for(int i = 0; i < N; i++){       // looping for the number of rows, columns, and boxes.
 
-        uint16_t row_mask = scan_row(board, i);                         // get the available candidates of each row.
+        uint16_t row_mask = scan_row(board, i);                                 // get the available candidates of each row.
         uint16_t col_mask = scan_col(board, i);
         uint16_t box_mask = scan_box(board, i);
 
-        for(int j = 0; j < N; j++){                                     // looping for the number of values in a row.
+        for(int j = 0; j < N; j++){                                             // looping for the number of values in a row.
             int index = j + i * N;
-            if((*board)->cells[index].remaining == 0) continue;
-            (*board)->cells[index].candidates |= row_mask;              // update the candidates
+            Cell *cell = &(*board)->cells[index];
+            if(cell->remainder == 0) continue;
+            cell->candidates |= row_mask;                      // update the candidates
+            cell->remainder = pop_count(cell->candidates); // update the remaining count
         }
 
-        for(int j = 0; j < N; j++){                                     // looping for the number of values in a column.
-            int index = i + j * N;    
-            if((*board)->cells[index].remaining == 0) continue;
-            (*board)->cells[index].candidates |= col_mask;              // update the candidates
+        for(int j = 0; j < N; j++){
+            int index = i + j * N;
+            Cell *cell = &(*board)->cells[index];    
+            if(cell->remainder == 0) continue;
+            cell->candidates |= col_mask;
+            cell->remainder = pop_count(cell->candidates);
         }
 
-        int box_row = (i / n) * n;                                      // row position of the top left of the box region
-        int box_column = (i % n) * n;                                   // column position of the top left of the box region
+        int box_row = (i / n) * n;                                              // row position of the top left of the box region
+        int box_column = (i % n) * n;                                           // column position of the top left of the box region
         
-        for(int j = 0; j < n; j++){                                     // looping for the number of values in a box.
+        for(int j = 0; j < n; j++){
             for(int k = 0; k < n; k++){
-                int box_x = box_row + j;                                // visualize it
+                int box_x = box_row + j;                                        // visualize it
                 int box_y = box_column + k;   
-                int index = box_x * N + box_y;           
-                if((*board)->cells[index].remaining == 0) continue;
-                (*board)->cells[index].candidates |= box_mask;          // update the candidates
+                int index = box_x * N + box_y;   
+                Cell *cell = &(*board)->cells[index];        
+                if(cell->remainder == 0) continue;
+                cell->candidates |= box_mask;
+                cell->remainder = pop_count(cell->candidates);
+                if(cell->remainder == 1) singles = true;
             }
         }
-    }    
+    }  
+    
+    return singles;
 }
 
-// CheckS all candidates to see if they produce a dead end.
-// return true if no dead end exists.
-bool forward_check(Board **board, int x, int y){
-    uint16_t candidates = (*board)->cells[x][y].candidates;
-    uint16_t neighbors = 0;
+/// Checks all candidates to see if they produce a dead end.
+/// return true if no dead end exists.
+bool forward_check(Board **board, int index){
+    int index = find_mrv_cell(board);
 
-    // check box validity
-    neighbors |= scan_row(board, x, y);
-    neighbors |= scan_column(board, x, y);
-    neighbors |= scan_box(board, x, y);
-
-    bool box_validity = (neighbors ^ candidates) == 511;
-
-    // check for neighbors
+    /// assume for now that the cell is valid
+    /// check cell neighbor validity
 
     bool neighbor_validity = check neighbor validity
 
@@ -70,7 +75,7 @@ bool forward_check(Board **board, int x, int y){
 }
 
 int* backtrack(int **board){
-    Cell cell = find_mrv_cell(board);  // fewest candidates
+    int index = find_mrv_cell(board);  // fewest candidates
 
     for digit in get_candidates(cell):
         if board is complete:
@@ -85,9 +90,11 @@ int* backtrack(int **board){
     return false
 }
 
-void solve(int **board){
-    constraint_propagation
-    backtrack()
+void solve(Board **board){
+    while(constraint_propagation(board)){
+        fill_singles(board);
+    }
+    backtrack() //not yet implemented
 }
 
 int main(int argc, char *argv[]){
